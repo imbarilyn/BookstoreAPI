e13cbd828855d48975f9deb9c3811bdb50527dfa
linah <linahmuhonjaimbari>
2024-02-11 13:45:06 +0300

    add index method on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :index]
    
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
