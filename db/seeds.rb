2cd17440958582edcc8b9329aed42db953a3a735
linah <linahmuhonjaimbari>
2024-02-11 15:15:20 +0300

    add  seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Book.destroy_all
+User.destroy_all
+
+5.times{Book.create(ttitle:Faker::Book.title, author:Faker::Book.author, publisher:Faker::Book.publisher, genre:Faker::Book.genre, synopsis: "lerem ipsum dgter ieuyeyi hyeyuuenijdygdujj")}
+5.times{User.create(username:Faker::Name.name, paassword: Faker::Name.name , confirm_password:Faker::Name.name)}
+
+puts "done seeding..."
