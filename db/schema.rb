c5a8a14a5bf210e92094421102b5b1fc864dd89b
linah <linahmuhonjaimbari>
2024-02-13 21:08:49 +0300

    add pblisher and author

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_11_180323) do
-  create_table "Books", force: :cascade do |t|
-    t.string "ttitle"
+ActiveRecord::Schema[7.1].define(version: 2024_02_13_173854) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"  
     t.string "author"
     t.string "publisher"
     t.datetime "year_of_publish"
     t.string "genre"
     t.text "synopsis"
+    t.integer "author_id"
+    t.integer "publisher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.string "title"   
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "paassword"
+    t.string "password"
     t.string "confirm_password"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "updated_at", null: false    
   end
-
 end
